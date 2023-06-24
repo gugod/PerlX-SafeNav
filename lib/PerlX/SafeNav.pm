fe0a35f7d716b8a8e1d8c733493f7688c6c0c6a7
Kang-min Liu <gugod@gugod.org>
2023-06-24 23:30:12 +0900

    more documentation.

--- lib/PerlX/SafeNav.pm
+++ lib/PerlX/SafeNav.pm
@@ -43,30 +43,54 @@ __END__
 
 PerlX::SafeNav - Safe-navigation for Perl
 
+=head1 SYNOPSIS
+
+Wrap a chain of method calls to make it resilient on encountering C<undef> values in the middle:
+
+    use PerlX::SafeNav ('$safenav', '$unsafenav');
+
+    my $tire = $car -> $safenv
+         -> wheels()
+         -> first()    # undef, if no wheels at all
+         -> tire()     # undef, if no tire on the wheel
+         -> remove()
+         -> $unsafenav;
+
+    unlessd (defined $tier) {
+        # The car either have no wheels, or the first wheel has no tire.
+        ...
+    }
+
 =head1 DESCRIPTION
 
-In many other languages, there is an operator (often C<?.>) for this purpose. For perl,
-there is currently an PPC: L<Optional Chaining|https://github.com/Perl/PPCs/blob/main/ppcs/ppc0021-optional-chaining-operator.md>
+In many other languages, there is an operator (often C<?.>) doing
+"Safe navigation", or "Optional Chaining".  The operator does a
+method-call when its left-hand side (first operant) is an object.  But
+when the left-hand side is an undefined value (or null value), the
+operator returns but evaluates to C<undef>.
+
+For perl there is currently an PPC: L<Optional Chaining|https://github.com/Perl/PPCs/blob/main/ppcs/ppc0021-optional-chaining-operator.md>
 
 This module provides a mean of making chains of method call safe
 regarding undef values. When encountering an C<undef> in the middle of
-the chain, the entier chain of calls is reduced to C<undef>.
+the chain, instead of dying with a "Can't call method on undefined
+value" method, the entier chain of calls is reduced to C<undef>.
 
-The implementation is done by inserting two extra methods surrounding a chain of method calls.
-
-Say, we want to do this:
+With C<PerlX::SaveNav> module, we could wrap a chain of calls and make
+it safe. Say, we want to do this:
 
     $ret = $o->a()->b()->c();
 
 ... but theose methods C<a()>, C<b()>, C<c()>, they all have some
 chances of returning C<undef>, perhaps as a way to signal the lack of
-data. Without proper checking, this chain of calls would end up
+data. Without proper checking, this chain of calls may end up
 erroring with a message like this:
 
     Can't call method "b" on an undefined value
 
-Instead of breaking the chain and adding C<defined> to check the
-return value after each method calls, we could do this instead:
+Instead of rewriting this nice little call chain into several
+statements and adding C<defined> to check the return value after each
+method calls, we could do this instead:
 
     use PerlX::SafeNav ('$safenav', '$unsafenav');
 
