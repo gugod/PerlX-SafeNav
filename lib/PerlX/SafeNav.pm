061422866435d9d28ecb12f088bde390c09effab
Kang-min Liu <gugod@gugod.org>
2023-06-24 21:42:09 +0900

    A fresh mint of PerlX::SafeNav

---
+++ lib/PerlX/SafeNav.pm
@@ -0,0 +1,36 @@
+package PerlX::SafeNav;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.000';
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf-8
+
+=head1 NAME
+
+PerlX::SafeNav - INSERT YOUR ABSTRACT HERE
+
+=head1 DESCRIPTION
+
+Write a full description of the module and its features here.
+
+=head1 AUTHOR
+
+Kang-min Liu <gugod@gugod.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2023 by Kang-min Liu.
+
+This is free software, licensed under:
+
+  The MIT (X11) License
+
+
